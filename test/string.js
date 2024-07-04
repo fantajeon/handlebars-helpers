10250c94df48b0c9f636f3be8f10e85525c3b853
mikesealey <mikesealey@hotmail.com>
2024-07-04 13:07:14 +0100

    adds loremIpsum function plus tests

--- test/string.js
+++ test/string.js
@@ -397,5 +397,50 @@ describe('string', function() {
       assert.equal(fn(), 'BENDER SHOULD NOT BE ALLOWED ON TV');
     });
   });
+
+  describe.only('lorem', function() {
+    // Bad parameters
+    it('Should return "Lorem ipsum" only, if passed no parameters', function() {
+      var fn = hbs.compile('{{ lorem }}');
+      assert.equal(fn(), "Lorem ipsum")
+    })
+    it('Should return "Lorem ipsum" only, if passed a non-number (string)', function() {
+      var fn = hbs.compile('{{ lorem a }}');
+      assert.equal(fn(), "Lorem ipsum")
+    }),
+    it('Should return "Lorem ipsum" only, if passed a non-number (array)', function() {
+      var fn = hbs.compile('{{ lorem [1,2,3] }}');
+      assert.equal(fn(), "Lorem ipsum")
+    }),
+    it('Should return "Lorem ipsum" only, if passed a number less than 1', function() {
+      var fn = hbs.compile('{{ lorem -1 }}');
+      assert.equal(fn(), "Lorem ipsum")
+    })
+    it('Should return "Lorem ipsum" only, if passed a number less than 1', function() {
+      var fn = hbs.compile('{{ lorem 0 }}');
+      assert.equal(fn(), "Lorem ipsum")
+    })
+    it('Should return "Lorem ipsum" only, if passed a number less than 1', function() {
+      var fn = hbs.compile('{{ lorem -999 }}');
+      assert.equal(fn(), "Lorem ipsum")
+    })
+    // Good parameters
+    it('Should return a string of "Lorem ipsum" if passed 11', function() {
+      var fn = hbs.compile('{{ lorem 11 }}');
+      assert.equal(fn(), "Lorem ipsum")
+    })
+    it('Should return a string of "Lorem" if passed 5', function() {
+      var fn = hbs.compile('{{ lorem 5 }}');
+      assert.equal(fn(), "Lorem")
+    })
+    it('Should return a string of "Lorem ipsum dolor sit amet, consectetur adipiscing" if passed 50', function() {
+      var fn = hbs.compile('{{ lorem 50 }}');
+      assert.equal(fn(), "Lorem ipsum dolor sit amet, consectetur adipiscing")
+    })
+    it('Should return a string of length 8032 if passed 8032', function() {
+      var fn = hbs.compile('{{ lorem 8032 }}');
+      assert.equal(fn().length, 8032)
+    })
+  })
 });
 
