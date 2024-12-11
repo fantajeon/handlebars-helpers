81eec3fff91a243a21e386621c04f21c95fbbf39
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2024-12-11 14:42:32 +0900

    package material ui

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {getBuildableComponents} = require('@google/dscc-scripts/build/viz/util');
+
+module.exports = {
+  // ... rest of your webpack configuration
+  entry: () => getBuildableComponents().reduce((acc, component) => {
+    acc[component.name] = `./${component.jsFile}`;
+    return acc;
+  }, {}),
+  // ... other configurations
+};
+
+const components = getBuildableComponents();
+const componentIndexToBuild = Number(process.env.WORKING_COMPONENT_INDEX) || 0;
+const component = components[componentIndexToBuild];
+
+console.log(`Building ${component.tsFile || component.jsFile}...`);
+
+const cssFilePath = path.resolve(__dirname, 'src', component.cssFile || '');
+const jsFilePath = path.resolve(__dirname, 'src', component.jsFile || '');
+
+const plugins = [
+  // Add DSCC_IS_LOCAL definition
+  new webpack.DefinePlugin({
+    DSCC_IS_LOCAL: 'true',
+  }),
+];
+
+let body = '<script src="main.js"></script>';
+if (fs.existsSync(cssFilePath)) {
+  body = body + '\n<link rel="stylesheet" href="index.css">';
+  plugins.push(new CopyWebpackPlugin([{from: cssFilePath, to: '.'}]));
+}
+const iframeHTML = `
+<!doctype html>
+<html><body>
+${body}
+</body></html>
+`;
+
+fs.writeFileSync(path.resolve(__dirname, 'dist', 'vizframe.html'), iframeHTML);
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: jsFilePath,
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: plugins,
+  },
+];
